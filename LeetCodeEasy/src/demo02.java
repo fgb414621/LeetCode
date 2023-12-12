f04208e1e04eb6336a6d86d7940035fd8b19db4c
fgb <1623323120@qq.com>
2023-12-12 14:46:32 +0800

    test:Demo02

--- LeetCodeEasy/src/demo02.java
+++ LeetCodeEasy/src/demo02.java
@@ -8,11 +8,20 @@ import javax.net.ssl.SSLServerSocket;
  * @Author fgb
  * @Create 2023/12/12 12:43
  * @Version 1.0
- *
+ * <p>
  * 27. 移除元素
  */
 public class demo02 {
     public static void main(String[] args) {
+        String s = "luffy is still joyboy";
+        lengthOfLastWord(s);
+    }
 
+
+    public static int lengthOfLastWord(String s) {
+        String[] words = s.split(" ");
+        //用空格拆分字符串
+        return words[words.length - 1].length();
+        //返回最后一个单词的长度
     }
 }
