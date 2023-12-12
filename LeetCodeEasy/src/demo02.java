d28b82f465351852c98823eacd405541c1913258
fgb <1623323120@qq.com>
2023-12-12 14:48:39 +0800

    test:Demo02

--- LeetCodeEasy/src/demo02.java
+++ LeetCodeEasy/src/demo02.java
@@ -4,7 +4,9 @@ import javax.net.ssl.SSLServerSocket;
  * ClassName: demo02
  * Package: PACKAGE_NAME
  * Description:
+ *给你一个字符串 s，由若干单词组成，单词前后用一些空格字符隔开。返回字符串中 最后一个 单词的长度。
  *
+ * 单词 是指仅由字母组成、不包含任何空格字符的最大子字符串。
  * @Author fgb
  * @Create 2023/12/12 12:43
  * @Version 1.0
