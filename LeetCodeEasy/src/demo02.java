57901498d15dcb499fba164725182dc041389bd6
fgb <1623323120@qq.com>
2023-12-12 12:46:17 +0800

    feat - 添加文件 1. demo2 力扣简单题: 第 27. 移除元素

---
+++ LeetCodeEasy/src/demo02.java
@@ -0,0 +1,18 @@
+import javax.net.ssl.SSLServerSocket;
+
+/**
+ * ClassName: demo02
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @Author fgb
+ * @Create 2023/12/12 12:43
+ * @Version 1.0
+ *
+ * 27. 移除元素
+ */
+public class demo02 {
+    public static void main(String[] args) {
+
+    }
+}
