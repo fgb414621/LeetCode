8306f6dc8d5e6fb1aef5da2d35453d449b7d25cc
fgb <1623323120@qq.com>
2023-12-13 11:47:47 +0800

    TestDemo03

--- LeetCodeEasy/src/Demo03.java
+++ LeetCodeEasy/src/Demo03.java
@@ -1,23 +1,33 @@
-import org.junit.Test;
 
 /**
  * ClassName: Demo03
  * Package: PACKAGE_NAME
  * Description:
- *编写一个函数来查找字符串数组中的最长公共前缀。
  *
- * 如果不存在公共前缀，返回空字符串 ""。
  * @Author fgb
- * @Create 2023/12/13 10:26
+ * @Create 2023/12/13 10:48
  * @Version 1.0
  */
 public class Demo03 {
     public static void main(String[] args) {
-
-    }
-    @Test
-    public void test(){
-
+        String[] strs = {"dog", "racecar", "car"};
+        System.out.println(longestCommonPrefix(strs));
     }
 
+    public static String longestCommonPrefix(String[] strs) {
+        String prefix = strs[0]; // 初始化最长公共前缀为数组的第一个字符串
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) { // 如果当前字符串不包含前缀，则缩短前缀
+                prefix = prefix.substring(0, prefix.length() - 1);
+                //  返回一个新的字符串,从0开始,截取到prefix.length() - 1的字符串
+                if (prefix.isEmpty()) {
+                    //如果prefix为空是true,返回""
+                    return "";
+                }
+            }
+        }
+        return prefix;
+    }
 }
+
+
