5a44155e46580e66f81a4acd5fcbf43bcea64d14
fgb <1623323120@qq.com>
2023-12-13 10:32:35 +0800

    测试用例

--- LeetCodeEasy/src/Demo03.java
+++ LeetCodeEasy/src/Demo03.java
@@ -4,7 +4,9 @@ import org.junit.Test;
  * ClassName: Demo03
  * Package: PACKAGE_NAME
  * Description:
+ *编写一个函数来查找字符串数组中的最长公共前缀。
  *
+ * 如果不存在公共前缀，返回空字符串 ""。
  * @Author fgb
  * @Create 2023/12/13 10:26
  * @Version 1.0
