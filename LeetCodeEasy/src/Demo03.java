c57560f6b8a1f3b6383c586182389cb02912fbe7
fgb <1623323120@qq.com>
2023-12-13 10:27:21 +0800

    测试用例

---
+++ LeetCodeEasy/src/Demo03.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+/**
+ * ClassName: Demo03
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @Author fgb
+ * @Create 2023/12/13 10:26
+ * @Version 1.0
+ */
+public class Demo03 {
+    public static void main(String[] args) {
+
+    }
+    @Test
+    public void test(){
+
+    }
+
+}
